42dae727c103115d2db78e977706476759d06e57
abhey <abhey.khurana77@gmail.com>
2024-08-02 12:31:12 +0530

    Refatored Code , Implemented Docker

--- controllers/blogConroller.js
+++ controllers/blogConroller.js
@@ -1,11 +1,11 @@
-import Blog from "../models/blog.js";
+import blog from "../models/BlogModel.js";
 // Add a new blog
 const addBlog = async (req, res) => {
   try {
     const { title, description, category } = req.body;
     const image = req.file ? req.file.path : null;
 
-    const newBlog = await Blog.create({ title, description, image, category });
+    const newBlog = await blog.create({ title, description, image, category });
     res.status(201).json(newBlog);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -16,11 +16,15 @@ const addBlog = async (req, res) => {
 const getBlogsByCategory = async (req, res) => {
   try {
     const { category } = req.params;
-    const blogs = await Blog.findAll({ where: { category } });
+    const blogs = await blog.findAll({ where: { category } });
     res.status(200).json(blogs);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export { addBlog, getBlogsByCategory };
+const testingRoutes = (req, res) => {
+  res.json("IT IS WORKING");
+};
+
+export { addBlog, getBlogsByCategory, testingRoutes };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 const router = Router();
-import { addBlog, getBlogsByCategory } from "../controllers/blogConroller.js";
-import { upload } from "../middleware/upload.js";
+import {
+  addBlog,
+  getBlogsByCategory,
+  testingRoutes,
+} from "../controllers/blogConroller.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 
 router.post("/blogs", upload, addBlog);
 router.get("/blogs/category/:category", getBlogsByCategory);
-
+router.get("/testing", testingRoutes);
 export default router;
